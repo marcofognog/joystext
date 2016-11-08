1ab52858f691dc28e5173133491c72c240c4df42
Marcao <marcofognog@gmail.com>
2016-11-07 22:57:23 -0200

    Barely works

--- ex1.c
+++ ex1.c
@@ -8,16 +8,35 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "Couldn't initialize SDL: %s\n", SDL_GetError());
         exit(1);
     }
-  puts("initializing SDL");
+
+  SDL_Joystick *joystick;
+
+  SDL_JoystickEventState(SDL_ENABLE);
+  joystick = SDL_JoystickOpen(0);
 
   printf("%i joysticks were found.\n\n", SDL_NumJoysticks() );
-    printf("The names of the joysticks are:\n");
+  printf("The names of the joysticks are:\n");
 
-    int i;
-    for( i=0; i < SDL_NumJoysticks(); i++ )
+  int i;
+  for( i=0; i < SDL_NumJoysticks(); i++ )
+  {
+    printf("    %s\n", SDL_JoystickName(i));
+  }
+
+  SDL_Event event;
+  while(1){
+  while(SDL_PollEvent(&event))
+  {
+    puts("eaaa");
+    switch(event.type)
     {
-        printf("    %s\n", SDL_JoystickName(i));
+      case SDL_JOYBUTTONDOWN:
+        puts("apertou!!!!!!");
+        break;
     }
+  }
+  }
+
   SDL_Quit();
   puts("quit sdl");
 
