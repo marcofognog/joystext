1b790646ad534a822fbf0e6077ca75c4cf980e87
Marcao <marcofognog@gmail.com>
2016-11-10 08:59:57 -0200

    Map more keys.

--- ex1.c
+++ ex1.c
@@ -26,6 +26,7 @@ void send_keycode(keysym){
   XTestFakeKeyEvent(display,val,1,0);
   XTestFakeKeyEvent(display,val,0,0);
   XFlush(display);
+  XCloseDisplay(display);
 }
 
 void send_keycode_modified(modifier, keysym){
@@ -41,42 +42,96 @@ void send_keycode_modified(modifier, keysym){
 
 void send_key(int *binary_buttons){
   int a[16] = {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-  int b[16] = {0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-  int c[16] = {0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0};
-  int d[16] = {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0};
-  int e[16] = {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0};
-  int f[16] = {0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0};
-  int g[16] = {0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0};
-  int h[16] = {0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0};
-  int i[16] = {0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0};
-  int j[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0};
-  int k[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0};
-  int l[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1};
+  int e[16] = {0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  int i[16] = {0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  int o[16] = {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0};
+  int u[16] = {0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0};
+  int b[16] = {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0};
+  int c[16] = {0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0};
+  int d[16] = {0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0};
+  int f[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0};
+  int g[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0};
+  int h[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1};
+  int j[16] = {0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0};
+
+  int k[16] = {0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0};
+  int l[16] = {0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0};
+  int m[16] = {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0};
+  int n[16] = {0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0};
+  int p[16] = {0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0};
+  int q[16] = {0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0};
+
+  int r[16] = {0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0};
+  int s[16] = {0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0};
+  int t[16] = {1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0};
+  int v[16] = {0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0};
+  int w[16] = {0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0};
+  int x[16] = {0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0};
+
+  int y[16] = {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1};
+  int z[16] = {0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1};
+  int space[16] = {0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1};
+  int enter[16] = {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1};
+  int backspace[16] = {0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1};
 
   if(memcmp(binary_buttons,a, sizeof(a)) == 0)
     send_keycode(XK_a);
-  if(memcmp(binary_buttons,b, sizeof(b)) == 0)
-    send_keycode(XK_b);
-  if(memcmp(binary_buttons,c, sizeof(c)) == 0)
-    send_keycode(XK_c);
-  if(memcmp(binary_buttons,d, sizeof(d)) == 0)
-    send_keycode(XK_d);
   if(memcmp(binary_buttons,e, sizeof(e)) == 0)
     send_keycode(XK_e);
-  if(memcmp(binary_buttons,f, sizeof(f)) == 0)
-    send_keycode(XK_f);
+  if(memcmp(binary_buttons,i, sizeof(i)) == 0)
+    send_keycode(XK_i);
+  if(memcmp(binary_buttons,o, sizeof(o)) == 0)
+    send_keycode(XK_o);
+  if(memcmp(binary_buttons,u, sizeof(u)) == 0)
+    send_keycode(XK_u);
+  if(memcmp(binary_buttons,d, sizeof(e)) == 0)
+    send_keycode(XK_d);
+  if(memcmp(binary_buttons,c, sizeof(c)) == 0)
+    send_keycode(XK_c);
+  if(memcmp(binary_buttons,j, sizeof(j)) == 0)
+    send_keycode(XK_j);
+  if(memcmp(binary_buttons,b, sizeof(h)) == 0)
+    send_keycode(XK_b);
   if(memcmp(binary_buttons,g, sizeof(g)) == 0)
     send_keycode(XK_g);
+  if(memcmp(binary_buttons,f, sizeof(f)) == 0)
+    send_keycode(XK_f);
   if(memcmp(binary_buttons,h, sizeof(h)) == 0)
     send_keycode(XK_h);
-  if(memcmp(binary_buttons,i, sizeof(h)) == 0)
-    send_keycode(XK_i);
-  if(memcmp(binary_buttons,j, sizeof(h)) == 0)
-    send_keycode(XK_j);
-  if(memcmp(binary_buttons,k, sizeof(h)) == 0)
+  if(memcmp(binary_buttons,k, sizeof(k)) == 0)
     send_keycode(XK_k);
-  if(memcmp(binary_buttons,l, sizeof(h)) == 0)
-    send_keycode_modified(XK_Shift_L, XK_z);
+  if(memcmp(binary_buttons,l, sizeof(l)) == 0)
+    send_keycode(XK_l);
+  if(memcmp(binary_buttons,m, sizeof(m)) == 0)
+    send_keycode(XK_m);
+  if(memcmp(binary_buttons,n, sizeof(n)) == 0)
+    send_keycode(XK_n);
+  if(memcmp(binary_buttons,p, sizeof(p)) == 0)
+    send_keycode(XK_p);
+  if(memcmp(binary_buttons,q, sizeof(q)) == 0)
+    send_keycode(XK_q);
+  if(memcmp(binary_buttons,r, sizeof(r)) == 0)
+    send_keycode(XK_r);
+  if(memcmp(binary_buttons,s, sizeof(s)) == 0)
+    send_keycode(XK_s);
+  if(memcmp(binary_buttons,t, sizeof(t)) == 0)
+    send_keycode(XK_t);
+  if(memcmp(binary_buttons,v, sizeof(v)) == 0)
+    send_keycode(XK_v);
+  if(memcmp(binary_buttons,w, sizeof(w)) == 0)
+    send_keycode(XK_w);
+  if(memcmp(binary_buttons,x, sizeof(x)) == 0)
+    send_keycode(XK_x);
+  if(memcmp(binary_buttons,y, sizeof(y)) == 0)
+    send_keycode(XK_y);
+  if(memcmp(binary_buttons,z, sizeof(z)) == 0)
+    send_keycode(XK_z);
+  if(memcmp(binary_buttons,space, sizeof(space)) == 0)
+    send_keycode(XK_space);
+  if(memcmp(binary_buttons,enter, sizeof(enter)) == 0)
+    send_keycode(XK_Return);
+  if(memcmp(binary_buttons,backspace, sizeof(backspace)) == 0)
+    send_keycode(XK_BackSpace);
 }
 
 
