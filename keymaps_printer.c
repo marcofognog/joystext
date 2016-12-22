a839fda5e0ecb75e8a7259e61fba294cb3d7d539
Marcao <marcofognog@gmail.com>
2016-12-22 18:09:40 -0200

    Improve keymap printer.

--- keymaps_printer.c
+++ keymaps_printer.c
@@ -4,6 +4,18 @@ void print_result(struct keymap keymaps[]){
   for(int l=0; l<number_of_lines; l++){
     print_binary(keymaps[l].binary_buttons);
     printf(",%i,%i,%i, mode:%i\n", keymaps[l].keycode1, keymaps[l].keycode2,keymaps[l].keycode3, keymaps[l].mode);
+    if((*keymaps[l].t_modified).repository !=0){
+      for(int y=0; y<(*keymaps[l].t_modified).size; y++){
+        printf(" - ");
+        print_binary((*keymaps[l].t_modified).repository[y].binary_buttons);
+        printf(",%i,%i,%i, mode:%i\n",
+            (*keymaps[l].t_modified).repository[y].keycode1,
+            (*keymaps[l].t_modified).repository[y].keycode2,
+            (*keymaps[l].t_modified).repository[y].keycode3,
+            (*keymaps[l].t_modified).repository[y].mode
+            );
+      }
+    }
   }
 }
 
