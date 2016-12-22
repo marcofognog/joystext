7b130ba08b1ee42702b2648586b82ff1a8f4c6cf
Marcao <marcofognog@gmail.com>
2016-12-22 19:10:57 -0200

    Only print if there something.

--- keymaps_printer.c
+++ keymaps_printer.c
@@ -4,7 +4,7 @@ void print_result(struct keymap keymaps[]){
   for(int l=0; l<number_of_lines; l++){
     print_binary(keymaps[l].binary_buttons);
     printf(",%i,%i,%i, mode:%i\n", keymaps[l].keycode1, keymaps[l].keycode2,keymaps[l].keycode3, keymaps[l].mode);
-    if((*keymaps[l].t_modified).repository !=0){
+    if((*keymaps[l].t_modified).repository !=0 && pressed_key(keymaps[l].binary_buttons) == 1){
       for(int y=0; y<(*keymaps[l].t_modified).size; y++){
         printf(" - ");
         print_binary((*keymaps[l].t_modified).repository[y].binary_buttons);
