69943ab9f9f265ba9b896a10dda38d0151265ac4
Marcao <marcofognog@gmail.com>
2017-01-01 19:35:51 -0200

    Extract functions to their own file.

--- main.c
+++ main.c
@@ -4,45 +4,11 @@
 #include <X11/extensions/XTest.h>
 #include <X11/keysym.h>
 #include "parse_config.c"
+#include "sends.c"
 
 int pointer_mode=0;
 int pointer_step = 1;
 SDL_Joystick *joystick;
-
-void send_keycode(keysym){
-  Display* display = XOpenDisplay(0);
-  int val = XKeysymToKeycode(display, keysym);
-  XTestFakeKeyEvent(display,val,1,0);
-  XTestFakeKeyEvent(display,val,0,0);
-  XFlush(display);
-  XCloseDisplay(display);
-}
-
-void send_keycode_modified(modifier, keysym){
-  Display* display = XOpenDisplay(0);
-  int keycode = XKeysymToKeycode(display, keysym);
-  int modcode = XKeysymToKeycode(display, modifier);
-  XTestFakeKeyEvent(display,modcode,1,0);
-  XTestFakeKeyEvent(display,keycode,1,0);
-  XTestFakeKeyEvent(display,keycode,0,0);
-  XTestFakeKeyEvent(display,modcode,0,0);
-  XFlush(display);
-}
-
-void send_keycode_mod_mod(mod1,mod2, keysym){
-  Display* display = XOpenDisplay(0);
-  int keycode = XKeysymToKeycode(display, keysym);
-  int modcode1 = XKeysymToKeycode(display, mod1);
-  int modcode2 = XKeysymToKeycode(display, mod2);
-
-  XTestFakeKeyEvent(display,modcode1,1,0);
-  XTestFakeKeyEvent(display,modcode2,1,0);
-  XTestFakeKeyEvent(display,keycode,1,0);
-  XTestFakeKeyEvent(display,keycode,0,0);
-  XTestFakeKeyEvent(display,modcode1,0,0);
-  XTestFakeKeyEvent(display,modcode2,0,0);
-  XFlush(display);
-}
 
 void move_pointer(int dest_x,int dest_y){
   Display* display = XOpenDisplay(0);
---
+++ sends.c
@@ -0,0 +1,39 @@
+#include "SDL.h"
+#include <X11/extensions/XTest.h>
+#include <X11/keysym.h>
+
+void send_keycode(keysym){
+  Display* display = XOpenDisplay(0);
+  int val = XKeysymToKeycode(display, keysym);
+  XTestFakeKeyEvent(display,val,1,0);
+  XTestFakeKeyEvent(display,val,0,0);
+  XFlush(display);
+  XCloseDisplay(display);
+}
+
+void send_keycode_modified(modifier, keysym){
+  Display* display = XOpenDisplay(0);
+  int keycode = XKeysymToKeycode(display, keysym);
+  int modcode = XKeysymToKeycode(display, modifier);
+  XTestFakeKeyEvent(display,modcode,1,0);
+  XTestFakeKeyEvent(display,keycode,1,0);
+  XTestFakeKeyEvent(display,keycode,0,0);
+  XTestFakeKeyEvent(display,modcode,0,0);
+  XFlush(display);
+}
+
+void send_keycode_mod_mod(mod1,mod2, keysym){
+  Display* display = XOpenDisplay(0);
+  int keycode = XKeysymToKeycode(display, keysym);
+  int modcode1 = XKeysymToKeycode(display, mod1);
+  int modcode2 = XKeysymToKeycode(display, mod2);
+
+  XTestFakeKeyEvent(display,modcode1,1,0);
+  XTestFakeKeyEvent(display,modcode2,1,0);
+  XTestFakeKeyEvent(display,keycode,1,0);
+  XTestFakeKeyEvent(display,keycode,0,0);
+  XTestFakeKeyEvent(display,modcode1,0,0);
+  XTestFakeKeyEvent(display,modcode2,0,0);
+  XFlush(display);
+}
+
