fe45804dd5feca6c18051f9c3d49596b90c0c65e
Marcao <marcofognog@gmail.com>
2017-01-04 22:17:21 -0200

    Ops, fix bad array attribution.

--- spec/replace/fetch_presses_from_js.c
+++ spec/replace/fetch_presses_from_js.c
@@ -4,9 +4,12 @@ void fetch_presses_from_js(int *bin_buttons, SDL_Joystick *joystick){
   char temp_buttons[18];
   gets(temp_buttons);
 
-
   for(int i=0; i<16;i++){
-    bin_buttons[i] = atoi(&temp_buttons[i]);
+    if(temp_buttons[i] == '1'){
+      bin_buttons[i] = 1;
+    }else{
+      bin_buttons[i] = 0;
+    }
   }
 
   return 0;
