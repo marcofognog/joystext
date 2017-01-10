228f05d0e4826ba07e70289d1c6ae0c29bed6071
Marcao <marcofognog@gmail.com>
2017-01-10 11:09:02 -0200

    Spec for key combinations.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 describe "integration" do
-  context 'for  simple release presses' do
+  context 'for release presses' do
     context 'given a config file' do
       config_content =<<END
 F1:a,00
@@ -48,6 +48,26 @@ END
 END
 
           expect(`cat #{input_file} | ./spec/bin/ex1 #{config_file}`).to eq(expected)
+        end
+
+        it 'combining keys' do
+          input_content =<<END
+1000000000000000
+1000000000000010
+1000000000000010
+0000000000000000
+1111111111111111
+END
+          input_file = "tmp/input"
+          savef(input_file, input_content)
+
+          expected =<<END
+41
+END
+          cmd = "cat #{input_file} | ./spec/bin/ex1 #{config_file}"
+          puts "Running: #{cmd}"
+          expect(`#{cmd}`).to eq(expected)
+
         end
       end
     end
