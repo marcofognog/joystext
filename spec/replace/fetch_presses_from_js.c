ef7f6c8543f905579d66742818d53f9b1142c340
Marcao <marcofognog@gmail.com>
2017-01-25 12:52:26 -0200

    Add method replacement.

--- spec/replace/fetch_presses_from_js.c
+++ spec/replace/fetch_presses_from_js.c
@@ -15,3 +15,17 @@ void fetch_presses_from_js(int *bin_buttons, SDL_Joystick *joystick){
   return 0;
 }
 
+void fetch_microsoft_xbox360(int *bin_buttons, SDL_Joystick *joystick){
+  char temp_buttons[18];
+  gets(temp_buttons);
+
+  for(int i=0; i<16;i++){
+    if(temp_buttons[i] == '1'){
+      bin_buttons[i] = 1;
+    }else{
+      bin_buttons[i] = 0;
+    }
+  }
+
+  return 0;
+}
