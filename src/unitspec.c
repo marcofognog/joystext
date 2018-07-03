9b689dbfcdbc959f796490fabe5e6b4751a63ae5
Marcao <marcofognog@gmail.com>
2018-07-03 18:55:17 -0300

    also dislay the type of press

--- src/unitspec.c
+++ src/unitspec.c
@@ -11,16 +11,17 @@
 void print_result(struct keymap keymaps[], int lines){
   for(int l=0; l<lines; l++){
     print_binary(keymaps[l].binary_buttons);
-    printf(",%i,%i,%i, mode:%i\n", keymaps[l].keycode1, keymaps[l].keycode2,keymaps[l].keycode3, keymaps[l].mode);
+    printf(",%i,%i,%i, mode:%i, type: %i\n", keymaps[l].keycode1, keymaps[l].keycode2,keymaps[l].keycode3, keymaps[l].mode, keymaps[l].onpress);
     if((*keymaps[l].t_modified).repository !=0 ){
       for(int y=0; y<(*keymaps[l].t_modified).size; y++){
         printf(" - ");
         print_binary((*keymaps[l].t_modified).repository[y].binary_buttons);
-        printf(",%i,%i,%i, mode:%i, pointer: %p\n",
+        printf(",%i,%i,%i, mode:%i, type:%i, pointer: %p\n",
             (*keymaps[l].t_modified).repository[y].keycode1,
             (*keymaps[l].t_modified).repository[y].keycode2,
             (*keymaps[l].t_modified).repository[y].keycode3,
             (*keymaps[l].t_modified).repository[y].mode,
+            (*keymaps[l].t_modified).repository[y].onpress,
             (*keymaps[l].t_modified).repository
             );
       }
